3f2a97b89ea67ccccbc3b5bf47ec46dc0a20046a
Anas Ansari <anasanzari@gmail.com>
2016-03-05 00:21:10 +0530

    Sql

--- foodweb.sql
+++ foodweb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
+-- version 4.2.11
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 16, 2016 at 07:18 PM
--- Server version: 10.1.8-MariaDB
--- PHP Version: 5.6.14
+-- Generation Time: Mar 04, 2016 at 07:49 PM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `foodweb`
@@ -26,12 +26,31 @@ SET time_zone = "+00:00";
 -- Table structure for table `feedback`
 --
 
-CREATE TABLE `feedback` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `feedback` (
+`id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `feedback` longtext NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`id`, `name`, `email`, `feedback`) VALUES
+(1, 'Anas', 'anas', 'Hi there'),
+(2, 'CHeck', 'check', 'Hello there..'),
+(3, 'CHeck', 'check', 'Hello there..'),
+(4, 'CHec', 'check', 'Hello there..'),
+(5, 'CHe', 'check', 'Hello there'),
+(6, 'Anas', 'Final', 'Hello'),
+(7, 'Anas', 'Final', 'Hello'),
+(8, 'Anas', 'Check', 'Yup'),
+(9, 'Anas', 'Check', 'Yup'),
+(10, '', '', ''),
+(11, '', '', ''),
+(12, '', '', ''),
+(13, '', '', '');
 
 -- --------------------------------------------------------
 
@@ -39,13 +58,57 @@ CREATE TABLE `feedback` (
 -- Table structure for table `items`
 --
 
-CREATE TABLE `items` (
-  `item_id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `items` (
+`item_id` int(11) NOT NULL,
   `restaurant_id` int(11) NOT NULL,
   `cuisine` varchar(100) NOT NULL,
   `name` varchar(100) NOT NULL,
   `price` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`item_id`, `restaurant_id`, `cuisine`, `name`, `price`) VALUES
+(2, 3, 'Coffee', 'Cappuchino', 40),
+(3, 3, 'Coffee', 'Expresso', 50),
+(6, 5, 'Burger', 'Beef Burger', 50),
+(7, 5, 'Burger', 'Chicken Burger', 200),
+(8, 5, 'Burger', 'Spicy Cheese Burger', 250),
+(9, 5, 'Juice', 'Lime Juice', 35),
+(10, 3, 'Coffee', 'Americano', 30),
+(11, 3, 'Cake', 'Blackberry Cheese Cake', 250),
+(12, 3, 'Cake', 'Lemon Cake', 200),
+(13, 2, 'Rice', 'Spicy Chicken Fried Rice', 350),
+(14, 2, 'Chicken', 'BBQ Chicken', 200),
+(15, 2, 'Noodles', 'Schezwan Egg Noodles', 200),
+(16, 2, 'Chicken', 'Chicken Manchurian', 175),
+(17, 2, 'Juice', 'Orange Juice', 35),
+(18, 2, 'Juice', 'Mango Juice', 50);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE IF NOT EXISTS `orders` (
+`id` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `rest_id` int(11) NOT NULL,
+  `address` varchar(150) NOT NULL,
+  `items` text NOT NULL,
+  `status` varchar(50) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `userid`, `rest_id`, `address`, `items`, `status`) VALUES
+(10, 1, 2, 'Bangalore, Karnataka', '[{"item_id":13,"name":"Spicy Chicken Fried Rice","price":350,"order":1},{"item_id":14,"name":"BBQ Chicken","price":200,"order":2}]', 'Delivered'),
+(13, 1, 2, 'C Hostel\nNIT Calicut', '[{"item_id":13,"name":"Spicy Chicken Fried Rice","price":350,"order":1},{"item_id":15,"name":"Schezwan Egg Noodles","price":200,"order":2}]', 'Processing');
 
 -- --------------------------------------------------------
 
@@ -53,11 +116,21 @@ CREATE TABLE `items` (
 -- Table structure for table `restaurant`
 --
 
-CREATE TABLE `restaurant` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `restaurant` (
+`id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
-  `place` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `place` varchar(100) NOT NULL,
+  `min_order` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `restaurant`
+--
+
+INSERT INTO `restaurant` (`id`, `name`, `place`, `min_order`) VALUES
+(2, 'Le Sante Cafe', 'Bangalore', 150),
+(3, 'Coffee House', 'Mumbai', 200),
+(5, 'Burger Launch', 'Calicut', 150);
 
 -- --------------------------------------------------------
 
@@ -65,14 +138,22 @@ CREATE TABLE `restaurant` (
 -- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `users` (
+`id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `phone` varchar(10) NOT NULL,
   `address` longtext NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `phone`, `address`, `email`, `password`) VALUES
+(1, 'Rahul M Menon', '9465212545', 'Koramangla,\r\nBangalore', 'rahul@gmail.com', '1234'),
+(2, 'Obi Wan Kenoobi', '9560124587', 'Mance Wille,\r\nPlanet Orbyn,\r\nTyrant Galaxy.', 'obiwan@gmail.com', '12345');
 
 --
 -- Indexes for dumped tables
@@ -82,28 +163,31 @@ CREATE TABLE `users` (
 -- Indexes for table `feedback`
 --
 ALTER TABLE `feedback`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `items`
 --
 ALTER TABLE `items`
-  ADD PRIMARY KEY (`item_id`),
-  ADD KEY `restaurant_id` (`restaurant_id`),
-  ADD KEY `restaurant_id_2` (`restaurant_id`);
+ ADD PRIMARY KEY (`item_id`), ADD KEY `restaurant_id` (`restaurant_id`), ADD KEY `restaurant_id_2` (`restaurant_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+ ADD PRIMARY KEY (`id`), ADD KEY `rest_id` (`rest_id`), ADD KEY `userid` (`userid`);
 
 --
 -- Indexes for table `restaurant`
 --
 ALTER TABLE `restaurant`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -113,22 +197,27 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `feedback`
 --
 ALTER TABLE `feedback`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=19;
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT for table `restaurant`
 --
 ALTER TABLE `restaurant`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- Constraints for dumped tables
 --
@@ -137,7 +226,14 @@ ALTER TABLE `users`
 -- Constraints for table `items`
 --
 ALTER TABLE `items`
-  ADD CONSTRAINT `foreign key` FOREIGN KEY (`restaurant_id`) REFERENCES `restaurant` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ADD CONSTRAINT `foreign key` FOREIGN KEY (`restaurant_id`) REFERENCES `restaurant` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`rest_id`) REFERENCES `restaurant` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
